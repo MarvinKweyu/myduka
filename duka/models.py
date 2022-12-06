a2253d0e86f6acb486ebde3a508f5fd8bbe91fc4
Marvin Kweyu <mkweyu1@gmail.com>
2022-12-06 22:55:37 +0300

    feat: init listing of products and categories

--- duka/models.py
+++ duka/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -13,6 +14,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("duka:product_list_by_category", args=[self.slug])
 
 
 class Product(models.Model):
@@ -33,5 +37,8 @@ class Product(models.Model):
         ordering = ("name",)
         index_together = (("id", "slug"),)
 
-        def __str__(self):
-            return self.name
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("duka:product_detail", args=[self.id, self.slug])
