2b19576ca4aea129a9728ce7787801b7b8f34ed9
Marvin Kweyu <mkweyu1@gmail.com>
2022-12-16 11:51:57 +0300

    feat: display cart details across all pages

--- myduka/settings.py
+++ myduka/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # local apps
     "duka",
     "cart",
+    "orders",
     # installed
     "django_seed",
 ]
@@ -68,6 +69,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "cart.context_processors.cart",
             ],
         },
     },
